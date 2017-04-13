82626f37757920814650e43745d89521e6c31d01
Cameron Pavao <cameronpavao@umass.edu>
2017-04-13 12:50:36 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -4,20 +4,9 @@ var testModule = require('./util.js');
 var reverseString = testModule.reverseString;
 var bodyParser = require('body-parser');
 
-app.get('/', function(req, res){
-  res.send("Hello World!");
-}
-);
 app.use(bodyParser.text());
 
-app.post('/reverse', function(req,res){
-  if(typeof(req.body) === 'string'){
-    var reveresed = reverseString(req.body);
-    res.send(reveresed);
-  } else {
-    res.status(400).end();
-  }
-});
+app.use(express.static('../client/build'));
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
